8b80cda4c3ed58604ca4c01ce889ddcde09286b8
david-rojo <davidrojotech@gmail.com>
2021-02-04 01:18:29 +0100

    initial spring project commit

---
+++ src/main/java/com/cloudapps/persistencia_relacional/PersistenciaRelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.cloudapps.persistencia_relacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaRelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaRelacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudapps/persistencia_relacional/PersistenciaRelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudapps.persistencia_relacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaRelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
