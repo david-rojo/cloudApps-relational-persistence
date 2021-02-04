879463084fedb8e97a7ba6ddd69b15a9499f25cd
david-rojo <davidrojotech@gmail.com>
2021-02-04 01:26:05 +0100

    renamed main class

--- src/main/java/com/cloudapps/persistencia_relacional/Application.java
+++ src/main/java/com/cloudapps/persistencia_relacional/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersistenciaRelacionalApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersistenciaRelacionalApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
